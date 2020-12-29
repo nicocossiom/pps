4ba7aaa64d4a4c81e41fd265e946c1683631398b
nicocossio <nicocossiom@gmail.com>
2020-12-29 17:45:50 +0100

    terminado casi 87/100 en la entrega

--- bashc/minientrega.sh
+++ bashc/minientrega.sh
@@ -1,23 +1,22 @@
 #! /bin/bash
-if  [ $# != 1 ]; then
+#Solo aceptamos un argumento 
+if  [ $# != 1 ] ; then
     echo "minientrega.sh: Error(EX_USAGE), uso incorrecto del mandato. \"Success\"" >&2
     echo "minientrega.sh+ Número de argumentos incorrecto" >&2
     exit 64
 else 
-    if [ $1 == "-help" ] || [ $1 == "-h" ]; then
+    if [ $1 == "--help" ] || [ $1 == "-h" ]; then
         echo "minientrega.sh: Uso: $0 {nombre_archivo} o -h/--help para la ayuda"
         echo "minientrega.sh: Copia ficheros a entregar de un directorio a otro de entrega"
         exit 0
     fi
 fi
-
+#Directorio accesible?
 if [ ! -d "$MINIENTREGA_CONF" ]; then   
     echo "minientrega.sh: Error, no se pudo realizar la entrega" >&2
     echo "minientrega.sh+ no es accesible el directorio \"$MINIENTREGA_CONF\"" >&2
     exit 64
 fi
-
-echo  "$MINIENTREGA_CONF/$1"
 
 if [ ! -f "$MINIENTREGA_CONF/$1" ]; then
     echo "minientrega.sh: Error, no se pudo realizar la entrega" >&2
@@ -25,15 +24,26 @@ if [ ! -f "$MINIENTREGA_CONF/$1" ]; then
     exit 66
 fi    
 
-#Cogemos las variavles de entorno dentro del fichero
+#Cogemos las variables de entorno dentro del fichero
 source $MINIENTREGA_CONF/$1
 
 for file in $MINIENTREGA_FICHEROS; do
-    if [ ! -r "$file" ];
+    if [ ! -r "$file" ]; then 
     echo "minientrega.sh: Error, no se pudo realizar la entrega" >&2
-    echo "minientrega.sh+ no es accesible el fichero \"$1\"" >&2
+    echo "minientrega.sh+ no es accesible el fichero \"$MINIENTREGA_FICHEROS\"" >&2
     exit 66
     fi
 done
 
-if [ ! -w  $MINIENTREGA_DESTINO ]
+if [ ! -d  $MINIENTREGA_DESTINO ] || ! mkdir -p "$MINIENTREGA_DESTINO/${USER}" > /dev/null 2>&1 ; then
+    echo "minientrega.sh: Error, no se pudo realizar la entrega"
+    echo "minientrega.sh+ no se pudo crear el subdirectorio de entrega en \"$MINIENTREGA_DESTINO\""
+    exit 73
+fi
+
+#Hacemos copia en bulk de los ficheros al destino*USER
+cp $MINIENTREGA_FICHEROS "${MINIENTREGA_DESTINO}/${USER}"
+exit 0
+
+ 
+
